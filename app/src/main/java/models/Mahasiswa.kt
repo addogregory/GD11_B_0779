7e7ccbd3e96ba3867d5e71371b2513b0af31ab3f
addogregory <greg.yodha05@gmail.com>
2022-11-17 07:43:54 +0700

    GIUDED

---
+++ app/src/main/java/models/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
